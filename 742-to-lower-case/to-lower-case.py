93a6e61b0a8c633bdf3cbef6875903c48ca14b57
Nowrin begum R <23ad269@stjosephs.ac.in>
2026-08-04 21:30:02 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (83.89%) - LeetSync

---
+++ 742-to-lower-case/to-lower-case.py
@@ -0,0 +1,9 @@
+class Solution:
+    def toLowerCase(self, s: str) -> str:
+        for char in s:
+            if char.upper():
+                e = s.lower()
+            return e
+
+      
+        
